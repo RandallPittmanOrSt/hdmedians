822bba0e7fdf44203b0f59dc3c81d1a3c837863c
Randall Pittman <pittmara@oregonstate.edu>
2024-06-04 10:00:26 -0700

    Fix Literal values for axis

--- hdmedians/geomedian.pyi
+++ hdmedians/geomedian.pyi
@@ -7,14 +7,14 @@ Float32Or64 = TypeVar("Float32Or64", np.float32, np.float64)
 
 def geomedian(
     X: NDArray[Float32Or64],
-    axis: Literal[1, 2] = 1,
+    axis: Literal[0, 1] = 1,
     eps: float = 1e-8,
     maxiters: int = 1000
 ) -> NDArray[Float32Or64]: ...
 
 def nangeomedian(
     X: NDArray[Float32Or64],
-    axis: Literal[1, 2] = 1,
+    axis: Literal[0, 1] = 1,
     eps: float = 1e-7,
     maxiters: int = 500
 ) -> NDArray[Float32Or64]: ...
--- hdmedians/medoid.pyi
+++ hdmedians/medoid.pyi
@@ -1,8 +1,13 @@
+from typing import Literal, TypeVar
+
 import numpy as np
 from numpy.typing import NDArray
-from typing import TypeVar
 
 _T = TypeVar("_T", bound=np.number)
 
-def medoid(a: NDArray[_T], axis: int = 1, indexonly: bool = False) -> NDArray[_T]: ...
-def nanmedoid(a: NDArray[_T], axis: int = 1, indexonly: bool = False) -> NDArray[_T]: ...
+def medoid(
+    a: NDArray[_T], axis: Literal[0, 1] = 1, indexonly: bool = False
+) -> NDArray[_T]: ...
+def nanmedoid(
+    a: NDArray[_T], axis: Literal[0, 1] = 1, indexonly: bool = False
+) -> NDArray[_T]: ...
